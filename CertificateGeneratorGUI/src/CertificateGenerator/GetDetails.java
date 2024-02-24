f3b5314ccb2deb00a30f30fdafc7333d40d7bda1
Joel Olubankole <codeninjascambridge+Joel@gmail.com>
2024-02-24 17:08:21 +0000

    Made it display the details entered
    I made it display the details I entered on another screen after clicking on generate button.

--- CertificateGeneratorGUI/src/CertificateGenerator/GetDetails.java
+++ CertificateGeneratorGUI/src/CertificateGenerator/GetDetails.java
@@ -7,19 +7,39 @@ import java.awt.event.*;
 
 class GetDetails extends JFrame{
     JLabel lbStudentName, lbStudentID, lbStudentCourse;
-    JTextField txtStudentName;
-    Font font_1;
+    JTextField txtStudentName, txtStudentID, txtStudentCourse;
+    Font font_1, font_2;
+    JButton btnGenerate;
+    String studentName, studentID, studentCourse;
     
     public GetDetails(){
-        font_1 = new Font(Font.SANS_SERIF,Font.PLAIN, 20);
+        font_1 = new Font(Font.SANS_SERIF,Font.PLAIN, 15);
+        font_2 = new Font(Font.SERIF, Font.BOLD, 20);
         
+        txtStudentName = new JTextField();
+        txtStudentID = new JTextField();
+        txtStudentCourse = new JTextField();
+
         lbStudentName = new JLabel(" Student's Name: ");
         lbStudentName.setFont(font_1);
+        lbStudentID = new JLabel("Student's ID: ");
+        lbStudentID.setFont(font_1);
+        lbStudentCourse = new JLabel("Student's Course: ");
+        lbStudentCourse.setFont(font_1);
         
-        setSize(500,600);
+        btnGenerate = new JButton("Generate");
+        btnGenerate.setFont(font_2);
+        btnGenerate.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                generateButtonAction(e);
+            }
+        });        
+        
+        setSize(760,600);
         setVisible(true);
         setTitle("Certificate Generator - Student Details");
-//        setResizable(false);
+        setResizable(true);
         
         addComponents();
         
@@ -28,11 +48,46 @@ class GetDetails extends JFrame{
     public void addComponents(){
         setLayout(null);
         
-        lbStudentName.setBounds(20,2,300,100);
+        lbStudentName.setBounds(18,2,300,100);
         add(lbStudentName);
         
-        txtStudentName.setBounds(50, 2, 150, 25);
+        txtStudentName.setBounds(150, 40, 250, 25);
         add(txtStudentName);
+        
+        lbStudentID.setBounds(20,60,300,100);
+        add(lbStudentID);
+        
+        txtStudentID.setBounds(130, 100,250,25);
+        add(txtStudentID);
+        
+        lbStudentCourse.setBounds(20,118,300,100);
+        add(lbStudentCourse);
+        
+        txtStudentCourse.setBounds(160,158,250,25);
+        add(txtStudentCourse);
+        
+        btnGenerate.setBounds(200,200,150,30);
+        add(btnGenerate);
+        
+        
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource() == btnGenerate){
+            generateButtonAction(e);
+        }
+       
+    }
+    
+    public void generateButtonAction(ActionEvent e){
+        JOptionPane.showMessageDialog(this, "Saved");
+        studentName = txtStudentName.getText();
+        studentID = txtStudentID.getText();
+        studentCourse = txtStudentCourse.getText();
+        
+        DisplayDetails.display(studentName, studentID, studentCourse);
+        
+        
     }
 }
