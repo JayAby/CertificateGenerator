6d35a79749f1ce96dede2ae365761a51a8f050a4
Joel Olubankole <codeninjascambridge+Joel@gmail.com>
2024-03-23 13:22:00 +0000

    Added a second template
    Want to make it so that, when you login, it gets your details and adds it to the signature and awardee field

--- CertificateGeneratorGUI/src/CertificateGenerator/SelectTemplate.java
+++ CertificateGeneratorGUI/src/CertificateGenerator/SelectTemplate.java
@@ -29,6 +29,7 @@ public class SelectTemplate extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
         lblTitle = new javax.swing.JLabel();
         lblTemplate1 = new javax.swing.JLabel();
+        lblTemplate2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -44,6 +45,13 @@ public class SelectTemplate extends javax.swing.JFrame {
             }
         });
 
+        lblTemplate2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/CertificateGenerator/CertificatesTemplate/Certificate Design Template2 (1).png"))); // NOI18N
+        lblTemplate2.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                lblTemplate2MouseClicked(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -55,8 +63,10 @@ public class SelectTemplate extends javax.swing.JFrame {
                         .addComponent(lblTitle, javax.swing.GroupLayout.PREFERRED_SIZE, 203, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addContainerGap()
-                        .addComponent(lblTemplate1, javax.swing.GroupLayout.PREFERRED_SIZE, 455, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(1315, Short.MAX_VALUE))
+                        .addComponent(lblTemplate1, javax.swing.GroupLayout.PREFERRED_SIZE, 455, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(98, 98, 98)
+                        .addComponent(lblTemplate2, javax.swing.GroupLayout.PREFERRED_SIZE, 455, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(998, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -64,8 +74,10 @@ public class SelectTemplate extends javax.swing.JFrame {
                 .addGap(22, 22, 22)
                 .addComponent(lblTitle)
                 .addGap(29, 29, 29)
-                .addComponent(lblTemplate1, javax.swing.GroupLayout.PREFERRED_SIZE, 334, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(643, Short.MAX_VALUE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(lblTemplate1, javax.swing.GroupLayout.PREFERRED_SIZE, 334, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblTemplate2, javax.swing.GroupLayout.PREFERRED_SIZE, 334, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(1006, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -148,6 +160,68 @@ public class SelectTemplate extends javax.swing.JFrame {
 
     }//GEN-LAST:event_lblTemplate1MouseClicked
 
+    private void lblTemplate2MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_lblTemplate2MouseClicked
+        // TODO add your handling code here:
+                try{
+            //Load the certificate template
+            BufferedImage certificate = ImageIO.read(new File("/Users/joel/Desktop/ARU/1/SoftwarePrinciples/Assessment/CertificateGenerator/CertificateGeneratorGUI/src/CertificateGenerator/CertificatesTemplate/CertificateDesignTemplate2.png"));
+            
+//            //Create a new Image with dimensions
+//            BufferedImage resizedImage = new BufferedImage(801,518,BufferedImage.TYPE_INT_ARGB);
+//            Graphics2D j = resizedImage.createGraphics();
+//            
+//            //draw the original image onto the resized image
+//            j.drawImage(certificate,0,0,801,518,null);
+//            j.dispose();
+            
+            //Create a graphic object from the image
+            Graphics2D g = certificate.createGraphics();
+            Graphics2D f = certificate.createGraphics();
+            g.setColor(Color.BLACK);
+            g.setFont(new Font("Arial",Font.BOLD,40));
+            f.setColor(Color.BLACK);
+            f.setFont(new Font("Times New Roman", Font.ITALIC, 50));
+            
+            //Draw the recipients name
+            String name = studentDetails[0] ;
+            String dept = studentDetails[1];
+            String id = studentDetails[2];
+            String startYear = studentDetails[3];
+            String endYear = studentDetails[4];
+            String awardDate = studentDetails[5];
+            String reasonForAward = studentDetails[6];
+            f.drawString(name, 900, 540);
+            g.drawString(dept,850,1100);
+            g.drawString(id,290,1100);
+            g.drawString(startYear +  " - " + endYear ,1400,1100);
+            g.drawString(awardDate, 900, 932); 
+            g.drawString(reasonForAward, 900,760); 
+            
+            g.dispose();
+            f.dispose();
+
+            
+            //Create the output folder if it doesn't exist
+            File folder = new File(OUTPUT_FOLDER);
+            if(!folder.exists()){
+                folder.mkdir();
+            }
+            
+            //Save the certificate as a PNG file
+            File pngOutputFile = new File(OUTPUT_FOLDER + File.separator + "certificate_" + name + id + ".png");
+            ImageIO.write(certificate, "png", pngOutputFile);
+
+            JOptionPane.showMessageDialog(this, "Certificate saved as PNG");
+           
+            
+        }catch(IOException e){
+            e.printStackTrace();
+            
+        }
+        
+        
+    }//GEN-LAST:event_lblTemplate2MouseClicked
+
     /**
      * @param args the command line arguments
      */
@@ -186,6 +260,7 @@ public class SelectTemplate extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel lblTemplate1;
+    private javax.swing.JLabel lblTemplate2;
     private javax.swing.JLabel lblTitle;
     // End of variables declaration//GEN-END:variables
 }
